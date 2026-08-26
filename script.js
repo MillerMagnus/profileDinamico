da8ca1298b1efaf6fa2de48253dda087614d22df
MillerMagnus <millermagnus@outlook.com>
2026-08-26 20:58:35 -0300

    codigo inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+const botaoAtualizar =
+document.getElementById("atualizar");
+const botaoTema =
+document.getElementById("tema");
+const campoNome =
+document.getElementById("novoNome");
+const nomePerfil =
+document.getElementById("nomePerfil");
+const profissao =
+document.getElementById("profissao");
+const mensagem =
+document.getElementById("mensagem");
+const perfil =
+document.querySelector(".perfil");
+
+
+
